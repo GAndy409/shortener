28620ef3affd162a7dd200bb6e6961f0ad3d66ee
Gubanov.Andrey2 <gubanov.andrey2@wb.ru>
2024-06-11 13:47:21 +0300

    ...

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/GAndy409/shortener/internal/app"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	app.HandleInit(mux)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/app/transport.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HandleInit(mux *http.ServeMux) {
+	mux.HandleFunc("/", Hello)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	jsonPresenter(w, "Hello")
+}
+
+func jsonPresenter(w http.ResponseWriter, v interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_ = json.NewEncoder(w).Encode(v)
+}
