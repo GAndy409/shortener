6a58cee34c65d0ec6d4f4fe1a02f892ec16adc0f
Gubanov.Andrey2 <gubanov.andrey2@wb.ru>
2024-06-11 15:27:39 +0300

    ...

--- internal/app/transport.go
+++ internal/app/transport.go
@@ -16,22 +16,22 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		responseString, err := rString(r)
 		if err != nil {
-			jsonPresenter(w, err)
+			jsonPresenter(w, http.StatusBadRequest, err)
 		}
 
 		sUrl := shorts.Shorts.ShortUrl(responseString)
-		jsonPresenter(w, sUrl)
+		jsonPresenter(w, http.StatusCreated, sUrl)
 	} else if r.Method == http.MethodGet {
 		responseString, err := rString(r)
 		if err != nil {
-			jsonPresenter(w, err)
+			jsonPresenter(w, http.StatusBadRequest, err)
 		}
 
 		search, fullUrl := shorts.Shorts.CheckUrl(responseString)
 		if search {
-			jsonPresenter(w, fullUrl)
+			jsonPresenter(w, http.StatusTemporaryRedirect, fullUrl)
 		} else {
-			jsonPresenter(w, "not found")
+			jsonPresenter(w, http.StatusBadRequest, "not found")
 		}
 	}
 }
@@ -45,9 +45,9 @@ func rString(r *http.Request) (string, error) {
 	return responseString, nil
 }
 
-func jsonPresenter(w http.ResponseWriter, v interface{}) {
+func jsonPresenter(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(status)
 
 	_ = json.NewEncoder(w).Encode(v)
 }
