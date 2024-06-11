597d660f855e39228d2a417f1cb373974351515b
Gubanov.Andrey2 <gubanov.andrey2@wb.ru>
2024-06-11 18:44:19 +0300

    ...

--- internal/app/transport.go
+++ internal/app/transport.go
@@ -11,34 +11,35 @@ import (
 func RouterInit() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", Hello)
-	router.HandleFunc("/{id}", Hello)
+	router.HandleFunc("/", ShortingPost).Methods(http.MethodPost)
+	router.HandleFunc("/{id}", ShortingGet).Methods(http.MethodGet)
 
 	return router
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		responseString, err := rString(r)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-		}
-
-		sUrl := shorts.Shorts.ShortUrl(responseString)
-		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "text/plain")
-		_, _ = w.Write([]byte(sUrl))
-	} else if r.Method == http.MethodGet {
-		shortUrl := mux.Vars(r)["id"]
-		search, fullUrl := shorts.Shorts.CheckShortKey(shortUrl)
-		if search {
-			w.Header().Set("Content-Type", "text/plain")
-			w.Header().Set("Location", fullUrl)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-		}
+func ShortingPost(w http.ResponseWriter, r *http.Request) {
+	responseString, err := rString(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 	}
+
+	sUrl := shorts.Shorts.ShortUrl(responseString)
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "text/plain")
+	_, _ = w.Write([]byte(sUrl))
+}
+
+func ShortingGet(w http.ResponseWriter, r *http.Request) {
+	shortUrl := mux.Vars(r)["id"]
+	search, fullUrl := shorts.Shorts.CheckShortKey(shortUrl)
+	if search {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Location", fullUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
 }
 
 func rString(r *http.Request) (string, error) {
